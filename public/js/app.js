2fcc52bee505904e2cd51c49e07ad941fcd177f2
iAmBentley <joey@moticorp.com>
2020-03-10 10:57:53 -0400

    gitignore + editorconfig clean up

--- public/js/app.js
+++ public/js/app.js
@@ -31609,8 +31609,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/wilbur/Sites/wantit/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/wilbur/Sites/wantit/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Users/bentley/Sites/wantit/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/bentley/Sites/wantit/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
